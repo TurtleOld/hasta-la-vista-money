75a0903be3f0a6d8ddc1ee31851382d74b03e6c2
Alexander Pavlov <dev@pavlovteam.ru>
2025-07-06 12:33:24 +0300

    🦄 refactor: Detected user input flowing into a manually constructed HTML string

--- hasta_la_vista_money/expense/views.py
+++ hasta_la_vista_money/expense/views.py
@@ -12,6 +12,7 @@ from django.shortcuts import get_object_or_404, redirect
 from django.template.loader import render_to_string
 from django.urls import reverse_lazy
 from django.utils.formats import date_format
+from django.utils.html import escape
 from django.views import View
 from django.views.generic import CreateView, DeleteView, DetailView, UpdateView
 from django.views.generic.list import ListView
@@ -524,45 +525,30 @@ class ExpenseDataAjaxView(LoginRequiredMixin, View):
 
         data = []
         for expense in expenses.select_related('category', 'account', 'user').all():
-            csrf_token = request.META.get('CSRF_COOKIE', '')
-            actions = f"""
-                <div class=\"table-buttons\">
-                    <a href=\"/expense/change/{expense.pk}/\" class=\"change-object-button btn btn-info border-0 btn-sm me-1\" title=\"Редактировать расход\">
-                        <svg xmlns=\"http://www.w3.org/2000/svg\" width=\"12\" height=\"12\" fill=\"currentColor\" class=\"bi bi-pencil-fill\" viewBox=\"0 0 16 16\">
-                            <path d=\"M12.854.146a.5.5 0 0 0-.707 0L10.5 1.793 14.207 5.5l1.647-1.646a.5.5 0 0 0 0-.708l-3-3zm.646 6.061L9.793 2.5 3.293 9H3.5a.5.5 0 0 1 .5.5v.5h.5a.5.5 0 0 1 .5.5v.5h.5a.5.5 0 0 1 .5.5v.5h.5a.5.5 0 0 1 .5.5v.207l6.5-6.5zm-7.468 7.468A.5.5 0 0 1 6 13.5V13h-.5a.5.5 0 0 1-.5-.5V12h-.5a.5.5 0 0 1-.5-.5V11h-.5a.5.5 0 0 1-.5-.5V10h-.5a.499.499 0 0 1-.175-.032l-.179.178a.5.5 0 0 0-.11.168l-2 5a.5.5 0 0 0 .65.65l5-2a.5.5 0 0 0 .168-.11l.178-.178z\"></path>
-                        </svg>
-                    </a>
-                    <form method=\"post\" action=\"/expense/{expense.pk}/copy/\" style=\"display:inline;\">
-                        <input type=\"hidden\" name=\"csrfmiddlewaretoken\" value=\"{csrf_token}\">
-                        <button class=\"btn btn-warning border-0 btn-sm me-1\" type=\"submit\" title=\"Скопировать расход\">
-                            <svg xmlns=\"http://www.w3.org/2000/svg\" width=\"16\" height=\"16\" fill=\"currentColor\" class=\"bi bi-copy\" viewBox=\"0 0 16 16\">
-                                <path fill-rule=\"evenodd\" d=\"M4 2a2 2 0 0 1 2-2h8a2 2 0 0 1 2 2v8a2 2 0 0 1-2 2H6a2 2 0 0 1-2-2zm2-1a1 1 0 0 0-1 1v8a1 1 0 0 0 1 1h8a1 1 0 0 0 1-1V2a1 1 0 0 0-1-1zM2 5a1 1 0 0 0-1 1v8a1 1 0 0 0 1 1h8a1 1 0 0 0 1-1v-1h1v1a2 2 0 0 1-2 2H2a2 2 0 0 1-2-2V6a2 2 0 0 1 2-2h1v1z\"></path>
-                            </svg>
-                        </button>
-                    </form>
-                    <form method=\"post\" action=\"/expense/delete/{expense.pk}/\" style=\"display:inline;\">
-                        <input type=\"hidden\" name=\"csrfmiddlewaretoken\" value=\"{csrf_token}\">
-                        <button class=\"remove-object-button btn btn-danger border-0 btn-sm\" type=\"submit\" name=\"delete_expense_button\" title=\"Удалить расход\">
-                            <svg xmlns=\"http://www.w3.org/2000/svg\" width=\"12\" height=\"12\" fill=\"currentColor\" class=\"bi bi-trash\" viewBox=\"0 0 16 16\">
-                                <path d=\"M5.5 5.5A.5.5 0 0 1 6 6v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5zm2.5 0a.5.5 0 0 1 .5.5v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5zm3 .5a.5.5 0 0 0-1 0v6a.5.5 0 0 0 1 0V6z\"></path>
-                                <path fill-rule=\"evenodd\" d=\"M14.5 3a1 1 0 0 1-1 1H13v9a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2V4h-.5a1 1 0 0 1-1-1V2a1 1 0 0 1 1-1H6a1 1 0 0 1 1-1h2a1 1 0 0 1 1-1h3.5a1 1 0 0 1 1 1v1zM4.118 4 4 4.059V13a1 1 0 0 0 1 1h6a1 1 0 0 0 1-1V4.059L11.882 4H4.118zM2.5 3V2h11v1h-11z\"></path>
-                            </svg>
-                        </button>
-                    </form>
-                </div>
-            """
+            context = {
+                'expense': expense,
+                'csrf_token': request.META.get('CSRF_COOKIE', ''),
+            }
+            actions = render_to_string(
+                'expense/_expense_actions.html',
+                context,
+                request=request,
+            )
+
             data.append(
                 [
                     expense.date.strftime('%B %Y'),
                     f'{expense.amount:,.2f}',
-                    expense.category.name if expense.category else '',
-                    expense.account.name_account if expense.account else '',
-                    expense.user.get_full_name() or expense.user.username
-                    if expense.user
-                    else '',
+                    escape(expense.category.name if expense.category else ''),
+                    escape(expense.account.name_account if expense.account else ''),
+                    escape(
+                        expense.user.get_full_name() or expense.user.username
+                        if expense.user
+                        else '',
+                    ),
                     actions,
                     expense.user.id,
-                    expense.user.username,
+                    escape(expense.user.username),
                 ],
             )
         return JsonResponse({'data': data})
