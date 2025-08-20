1896988d36b36b39ffe30d52bab0d4cd0c1fbbd4
Alexander Pavlov <alexander.pavlov@pavlovteam.ru>
2025-08-20 18:13:15 +0300

    Revert "🐞 fix: docker build"

--- docker/entrypoint.sh
+++ docker/entrypoint.sh
@@ -13,6 +13,7 @@ echo "Database is up - executing migrations"
 
 echo "Creating staticfiles directory with proper permissions"
 mkdir -p /app/staticfiles
+chmod -R 755 /app/staticfiles
 
 echo "Collecting static files"
 .venv/bin/python manage.py collectstatic --noinput --clear
