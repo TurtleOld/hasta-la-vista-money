29101076b3b68c29e1fecbe69a54e2fd062598c2
Alexander Pavlov <dev@pavlovteam.ru>
2025-10-17 14:18:59 +0300

    change validate config env

--- config/django/validator_env.py
+++ config/django/validator_env.py
@@ -1,4 +1,4 @@
-from decouple import config
+from os import environ
 from icecream import ic
 
 
@@ -6,16 +6,16 @@ class EnvironmentValidator:
     def validate(self) -> bool:
         valid = True
 
-        if not config("SECRET_KEY"):
+        if not environ.get('SECRET_KEY'):
             valid = False
-            ic("SECRET_KEY is not set, use make secretkey to generate it")
-        if not config("DEBUG"):
+            ic('SECRET_KEY is not set, use make secretkey to generate it')
+        if not environ.get('DEBUG'):
             valid = False
-            ic("DEBUG is not set, set it to true or false")
-        if not config("ALLOWED_HOSTS"):
+            ic('DEBUG is not set, set it to true or false')
+        if not environ.get('ALLOWED_HOSTS'):
             valid = False
-            ic("ALLOWED_HOSTS is not set, set it to a comma-separated list of hosts")
-        if not config("DATABASE_URL"):
+            ic('ALLOWED_HOSTS is not set, set it to a comma-separated list of hosts')
+        if not environ.get('DATABASE_URL'):
             valid = False
-            ic("DATABASE_URL is not set, set it to a valid database URL")
+            ic('DATABASE_URL is not set, set it to a valid database URL')
         return valid
