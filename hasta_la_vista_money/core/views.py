930f96f7cc0de809b4b69c298e5c364285d87f3c
Alexander Pavlov <dev@pavlovteam.ru>
2025-11-26 19:45:12 +0300

    refactor: улучшена типизация базовых view классов
    - Добавлена типизация для SuccessMessageMixin
    - Исправлены аннотации типов для BaseEntityCreateView и BaseEntityUpdateView
    - Добавлены type: ignore комментарии для корректной работы mypy

--- hasta_la_vista_money/core/views.py
+++ hasta_la_vista_money/core/views.py
@@ -3,7 +3,7 @@
 from django.contrib.auth.mixins import LoginRequiredMixin
 from django.contrib.messages.views import SuccessMessageMixin
 from django.db import models
-from django.forms import ModelForm
+from django.forms import BaseForm
 from django.urls import reverse_lazy
 from django.views.generic import CreateView, UpdateView
 from django_filters.views import FilterView
@@ -11,17 +11,24 @@ from django_filters.views import FilterView
 from hasta_la_vista_money import constants
 
 
-class BaseEntityFilterView(LoginRequiredMixin, SuccessMessageMixin, FilterView):
+class BaseEntityFilterView(
+    LoginRequiredMixin,
+    SuccessMessageMixin[BaseForm],
+    FilterView,
+):
     """Common configuration for entity filter views."""
 
     paginate_by: int | None = constants.PAGINATE_BY_DEFAULT
     no_permission_url = reverse_lazy('login')
 
 
-class BaseEntityCreateView[ModelType: models.Model, FormType: ModelForm](
+class BaseEntityCreateView[  # type: ignore[misc]
+    ModelType: models.Model,
+    FormType,
+](
     LoginRequiredMixin,
-    SuccessMessageMixin,
-    CreateView[ModelType, FormType],
+    SuccessMessageMixin[BaseForm],
+    CreateView[ModelType, FormType],  # type: ignore[type-var]
 ):
     """Base class for creating entities.
 
@@ -32,10 +39,13 @@ class BaseEntityCreateView[ModelType: models.Model, FormType: ModelForm](
     no_permission_url = reverse_lazy('login')
 
 
-class BaseEntityUpdateView[ModelType: models.Model, FormType: ModelForm](
+class BaseEntityUpdateView[  # type: ignore[misc]
+    ModelType: models.Model,
+    FormType,
+](
     LoginRequiredMixin,
-    SuccessMessageMixin,
-    UpdateView[ModelType, FormType],
+    SuccessMessageMixin[BaseForm],
+    UpdateView[ModelType, FormType],  # type: ignore[type-var]
 ):
     """Base class for updating entities.
 
