94dedefd9912f8980bb688cd67498c59edf3fd4b
Alexander Pavlov <dev@pavlovteam.ru>
2025-12-15 13:51:37 +0300

    feat: update finance account API and serializers

--- hasta_la_vista_money/finance_account/serializers.py
+++ hasta_la_vista_money/finance_account/serializers.py
@@ -6,6 +6,7 @@ finance account models to and from JSON format for API communication.
 
 from typing import Any, ClassVar
 
+from django.urls import reverse
 from rest_framework import serializers
 
 from hasta_la_vista_money.finance_account.models import Account
@@ -18,6 +19,18 @@ class AccountSerializer(serializers.ModelSerializer[Account]):
     including essential account information like name, balance, and currency.
     """
 
+    type_account_display = serializers.CharField(
+        source='get_type_account_display',
+        read_only=True,
+    )
+    user_username = serializers.CharField(
+        source='user.username',
+        read_only=True,
+    )
+    url = serializers.SerializerMethodField()
+    delete_url = serializers.SerializerMethodField()
+    is_foreign = serializers.SerializerMethodField()
+
     class Meta:
         model = Account
         fields: ClassVar[list[str]] = [
@@ -26,12 +39,33 @@ class AccountSerializer(serializers.ModelSerializer[Account]):
             'balance',
             'currency',
             'type_account',
+            'type_account_display',
             'bank',
             'limit_credit',
             'payment_due_date',
             'grace_period_days',
+            'user_username',
+            'url',
+            'delete_url',
+            'is_foreign',
         ]
         read_only_fields: ClassVar[list[str]] = ['id']
+
+    def get_url(self, obj: Account) -> str:
+        """Get absolute URL for account edit."""
+        return obj.get_absolute_url()
+
+    def get_delete_url(self, obj: Account) -> str:
+        """Get URL for account deletion."""
+
+        return reverse('finance_account:delete_account', args=[obj.pk])
+
+    def get_is_foreign(self, obj: Account) -> bool:
+        """Check if account belongs to another user."""
+        request = self.context.get('request')
+        if request and hasattr(request, 'user'):
+            return obj.user != request.user
+        return False
 
     def create(self, validated_data: dict[str, Any]) -> Account:
         """Override create to set user from request."""
