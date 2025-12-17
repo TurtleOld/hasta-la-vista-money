d730a5ce42b15709a8d514a7491449d5722c13f7
Alexander Pavlov <dev@pavlovteam.ru>
2025-12-17 21:59:50 +0300

    РћР±РЅРѕРІР»РµРЅРёРµ РјРѕРґСѓР»СЏ С‡РµРєРѕРІ: views Рё С€Р°Р±Р»РѕРЅС‹

--- hasta_la_vista_money/receipts/views.py
+++ hasta_la_vista_money/receipts/views.py
@@ -14,7 +14,7 @@ from hasta_la_vista_money.core.types import RequestWithContainer
 
 if TYPE_CHECKING:
     from django.forms import ModelChoiceField
-from django.http import Http404, HttpRequest, HttpResponse
+from django.http import Http404, HttpRequest, HttpResponse, JsonResponse
 from django.shortcuts import get_object_or_404, redirect
 from django.urls import reverse_lazy
 from django.utils.translation import gettext_lazy
@@ -207,15 +207,41 @@ class SellerCreateView(
     model = Seller
     form_class: type[SellerForm] = SellerForm
 
+    def _wants_json_response(self) -> bool:
+        requested_with = self.request.headers.get('x-requested-with', '')
+        if requested_with.lower() == 'xmlhttprequest':
+            return True
+        hx_request = self.request.headers.get('hx-request', '')
+        return hx_request.lower() == 'true'
+
     def form_valid(self, form: SellerForm) -> HttpResponse:
-        seller = form.save(commit=False)
-        seller.user = cast('User', self.request.user)
-        seller.save()
+        form.instance.user = cast('User', self.request.user)
+
+        if self._wants_json_response():
+            self.object = form.save()
+            messages.success(
+                self.request,
+                constants.SUCCESS_MESSAGE_CREATE_SELLER,
+            )
+            return JsonResponse({'success': True})
+
+        response = super().form_valid(form)
         messages.success(
             self.request,
             constants.SUCCESS_MESSAGE_CREATE_SELLER,
         )
-        return super().form_valid(form)
+        return response
+
+    def form_invalid(self, form: SellerForm) -> HttpResponse:
+        if self._wants_json_response():
+            return JsonResponse(
+                {
+                    'success': False,
+                    'errors': form.errors,
+                },
+                status=400,
+            )
+        return super().form_invalid(form)
 
     def get_success_url(self) -> str:
         return str(reverse_lazy('receipts:list'))
