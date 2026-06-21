c422f5e8ed9be187c1227ddda2533ed0f478b895
Alexander Pavlov <dev@pavlovteam.ru>
2026-06-21 16:07:24 +0300

    feat(users): add PII stripper for bank statement descriptions

---
+++ hasta_la_vista_money/users/tests/test_pii_stripper.py
@@ -0,0 +1,23 @@
+from django.test import TestCase
+
+from hasta_la_vista_money.users.services.pii_stripper import strip_pii
+
+
+class TestStripPii(TestCase):
+    def test_removes_masked_card(self):
+        assert strip_pii('Оплата *4321 MAGNIT') == 'Оплата MAGNIT'
+
+    def test_removes_auth_code(self):
+        assert strip_pii('NETFLIX.COM 123456') == 'NETFLIX.COM'
+
+    def test_removes_date_fragment(self):
+        assert strip_pii('SPAR 12.03.2025 Москва') == 'SPAR Москва'
+
+    def test_preserves_clean_description(self):
+        assert strip_pii('Продукты питания') == 'Продукты питания'
+
+    def test_collapses_extra_spaces(self):
+        assert strip_pii('Яндекс  Такси  *1234') == 'Яндекс Такси'
+
+    def test_empty_string(self):
+        assert strip_pii('') == ''
