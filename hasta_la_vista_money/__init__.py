3d33fe768025893e7e872746032837e75d59d5e1
Alexander Pavlov <dev@pavlovteam.ru>
2026-07-04 20:03:03 +0300

    chore(main): release 2.8.0 (#878)

--- hasta_la_vista_money/__init__.py
+++ hasta_la_vista_money/__init__.py
@@ -1,4 +1,4 @@
-__version__ = '2.7.2'  # x-release-please-version
+__version__ = '2.8.0'  # x-release-please-version
 
 # This will make sure the app is always imported when
 # Django starts so that shared_task will use this app.
